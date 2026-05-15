aba323e84802b65e2d12928cf7d2e0e2c770bf69
jaheershaikh <jaheershaikh067@gmail.com>
2026-05-15 11:13:59 +0530

    add condition

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,30 +1,48 @@
-import { useState } from "react"
+import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 export default function TodoApp() {
-    const [todos, setTodos] = useState([{task: "sample task", id: uuidv4()}]);
-    const [newTodo, setNewTodo] = useState("")
+  const [todos, setTodos] = useState([{ task: "sample task", id: uuidv4() }]);
+  const [newTodo, setNewTodo] = useState("");
 
-    let addTask = () => {
-      setTodos([...todos, {task: newTodo, id: uuidv4() }])
-    }
-    let addNewTodo = (event) => {
-        setNewTodo(event.target.value);
-    }
-    return (
-        <div>
-            <h2>Todo App</h2>
-            <input type="text" placeholder="Add Todo" id="text" value={newTodo} onChange={addNewTodo}/>
-            <br /><br />
-            <button onClick={addTask}>Add</button>
-            <h3>Todos</h3>
-            <hr />
+  let addTask = () => {
+    // if(newTodo.trim() == "") {
+    //     alert("Input can't be empty")
+    // } else {
+    //     setTodos([...todos, {task: newTodo, id: uuidv4() }])
+    // }
 
-            <ul>
-                {todos.map((todo) => (
-                 <li key={todo.id}>{todo.task}</li>
-            ))}
-            </ul>
-        </div>
-    )
+    newTodo.trim() === ""
+      ? alert("input can't be empty")
+      : setTodos([...todos, { task: newTodo, id: uuidv4() }]);
+
+    setNewTodo("");
+  };
+
+  let addNewTodo = (event) => {
+    setNewTodo(event.target.value);
+  };
+  return (
+    <div>
+      <h2>Todo App</h2>
+      <input
+        type="text"
+        placeholder="Add Todo"
+        id="text"
+        value={newTodo}
+        onChange={addNewTodo}
+      />
+      <br />
+      <br />
+      <button onClick={addTask}>Add</button>
+      <h3>Todos</h3>
+      <hr />
+
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.task}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
